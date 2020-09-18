31b058be50f2d5ca6d8010a26fc98826d31dcece
fabricio.coimbra <fabriciob.coimbra@gmail.com>
2020-09-18 00:57:13 -0300

    Alterado a lista de veículas para um ListView de verdade com Binding para a classe de veículos.

---
+++ Xamarin 01/App01/App1/App1/Veiculo.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace App1
+{
+    public class Veiculo
+    {
+        public string Nome { get; set; }
+        public string Preco { get; set; }
+    }
+}
