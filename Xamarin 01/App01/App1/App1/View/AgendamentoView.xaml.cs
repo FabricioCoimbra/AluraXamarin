735d4efd5c8a2a9041178ca6ac85ab07969ef94d
fabricio.coimbra <fabriciob.coimbra@gmail.com>
2020-09-28 13:48:12 -0300

    Criado Agendamento básico

--- Xamarin 01/App01/App1/App1/View/AgendamentoView.xaml.cs
+++ Xamarin 01/App01/App1/App1/View/AgendamentoView.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using App1.Model;
+using System;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -7,12 +8,30 @@ namespace App1.View
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class AgendamentoView : ContentPage
     {
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public string Telefone { get; set; }
+        public DateTime DataAgendamento { get; set; }
+        public TimeSpan HoraAgendamento { get; set; }
         public Veiculo Veiculo { get; set; }
         public AgendamentoView(Veiculo veiculo)
         {
             InitializeComponent();
             Veiculo = veiculo;
             Title = Veiculo.Nome;
+            DataAgendamento = DateTime.Now.AddDays(3);
+            BindingContext = this;
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            DisplayAlert("Agendamento", string.Format(
+                @"Nome = {0}
+Fone: {1}
+E-mail: {2}
+Data Agendamento: {3}
+Hora Agendamento: {4}",
+                Nome, Telefone, Email, DataAgendamento.ToString("dd/MM/yyyy"), HoraAgendamento), "OK");
         }
     }
 }
