24016fb38b655bd44a0cfe7b6c4bbc84de826d6e
fabricio.coimbra <fabriciob.coimbra@gmail.com>
2020-09-28 08:45:15 -0300

    Adicionados bindings de variáveis locais (tudo uma bagunça direto na view por enquanto)

--- Xamarin 01/App01/App1/App1/View/DetalheView.xaml.cs
+++ Xamarin 01/App01/App1/App1/View/DetalheView.xaml.cs
@@ -8,12 +8,34 @@ namespace App1.View
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class DetalheView : ContentPage
     {
+        private const int FREIO_ABS = 800;
+        private const int CONTROLE_TRACAO = 1200;
+        private const int ASSISTENTE_PARTIDA_RAMPA = 1500;
+        private bool temFreioABS;
+
+        public String TextoFreioABS { get => $"Freio ABS - R$ {FREIO_ABS}"; }
+        public String TextoControleTracao { get => $"Controle de tração - R$ {CONTROLE_TRACAO}"; }
+        public String TextoAssistentePartidaRampa { get => $"Assitente de partida em rampa - R$ {ASSISTENTE_PARTIDA_RAMPA}"; }
+        public String TextoTotal { get => $"Total = R$ {Veiculo.Preco}"; }
+        public bool TemFreioABS
+        {
+            get => temFreioABS;
+            set
+            {
+                temFreioABS = value;
+                if (temFreioABS)
+                    DisplayAlert("Freio ABS", "Ligado", "OK");
+                else
+                    DisplayAlert("Freio ABS", "Desligado", "OK");
+            }
+        }
         public Veiculo Veiculo { get; set; }
         public DetalheView(Veiculo veiculo)
         {
             InitializeComponent();
             Veiculo = veiculo;
             Title = Veiculo.Nome;
+            BindingContext = this;
         }
 
         private void Button_Clicked(object sender, EventArgs e)
