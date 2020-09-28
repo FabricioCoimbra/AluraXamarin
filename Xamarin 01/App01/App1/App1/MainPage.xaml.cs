eede3306e05d78c9e8d67857a47f487e234e7ed3
fabricio.coimbra <fabriciob.coimbra@gmail.com>
2020-09-27 22:21:59 -0300

    Alterado para a classe veículo formatar o preço e associado evento de clique no veículo exibindo um alerta personalizado

--- Xamarin 01/App01/App1/App1/MainPage.xaml.cs
+++ Xamarin 01/App01/App1/App1/MainPage.xaml.cs
@@ -17,24 +17,30 @@ namespace App1
 
             this.Veiculos = new List<Veiculo>
             {
-                new Veiculo{ Nome="Sandero", Preco="R$ 54.290"},
-                new Veiculo{ Nome="Kwid", Preco="R$ 37.490"},
-                new Veiculo{ Nome="Stepway :)", Preco="R$ 70.090"},
-                new Veiculo{ Nome="Sandero RS", Preco="R$ 59.390"},
-                new Veiculo{ Nome="Sandero Vibe", Preco="R$ 59.390"},
-                new Veiculo{ Nome="Sandero GT Line", Preco="R$ 59.390"},
-                new Veiculo{ Nome="Logan", Preco="R$ 58.490"},
-                new Veiculo{ Nome="Captur", Preco="R$ 97.990"},
-                new Veiculo{ Nome="Duster", Preco="R$ 71.790"},
-                new Veiculo{ Nome="Oroch", Preco="R$ 69.590"},
-                new Veiculo{ Nome="Master", Preco="R$ 144.490"}
+                new Veiculo{ Nome="Sandero", Preco="54.290"},
+                new Veiculo{ Nome="Kwid", Preco="37.490"},
+                new Veiculo{ Nome="Stepway :)", Preco="70.090"},
+                new Veiculo{ Nome="Sandero RS", Preco="59.390"},
+                new Veiculo{ Nome="Sandero Vibe", Preco="59.390"},
+                new Veiculo{ Nome="Sandero GT Line", Preco="59.390"},
+                new Veiculo{ Nome="Logan", Preco="58.490"},
+                new Veiculo{ Nome="Captur", Preco="97.990"},
+                new Veiculo{ Nome="Duster", Preco="71.790"},
+                new Veiculo{ Nome="Oroch", Preco="69.590"},
+                new Veiculo{ Nome="Master", Preco="144.490"}
             };
 
-            //definido pelo XAML
+            //definido pelo XAML em ItemsSource="{Binding Veiculos}"
             //ListaVeiculos.ItemsSource = Veiculos;
 
             //Poderia ser definida outra classe para contexto de binding desacoplando, ou complicando um pouco mais as coisas ...
             BindingContext = this;
+        }
+
+        private void ListaVeiculos_ItemTapped(object sender, ItemTappedEventArgs e)
+        {
+            var veiculo = (Veiculo)e.Item;
+            DisplayAlert("Veículo selecionado", $"Voçê selecionou o veículo {veiculo.Nome} que custa {veiculo.Preco} ", "Entendi");
         }
     }
 }
--- Xamarin 01/App01/App1/App1/Veiculo.cs
+++ Xamarin 01/App01/App1/App1/Veiculo.cs
@@ -6,7 +6,9 @@ namespace App1
 {
     public class Veiculo
     {
+        private string preco;
+
         public string Nome { get; set; }
-        public string Preco { get; set; }
+        public string Preco { get => $"R$ {preco}"; set => preco = value; }
     }
 }
