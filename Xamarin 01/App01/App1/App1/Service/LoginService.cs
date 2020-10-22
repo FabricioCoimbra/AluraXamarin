2411d10428d033a19a4a26566de5357eec593864
fabricio.coimbra <fabriciob.coimbra@gmail.com>
2020-10-21 22:32:42 -0300

    Adicionado tratamento de erro de conexão simplificado.

--- Xamarin 01/App01/App1/App1/Service/LoginService.cs
+++ Xamarin 01/App01/App1/App1/Service/LoginService.cs
@@ -11,18 +11,26 @@ namespace App1.Service
     {
         public async Task FazerLogin(Login login)
         {
-            var campos = new FormUrlEncodedContent(new[]
-                    {
+            try
+            {
+                var campos = new FormUrlEncodedContent(new[]
+                            {
                             new KeyValuePair<string, string>("email", login.Email),
                             new KeyValuePair<string, string>("senha", login.Senha)
                         }
-                );
-            var response = await Client.PostAsync("/login", campos);
-            if (response.IsSuccessStatusCode)
-                MessagingCenter.Send<Usuario>(new Usuario(), "LoginSucesso");
-            else
-                MessagingCenter.Send<LoginException>(new LoginException("Erro ao autenticar o usuário \n" + await response.Content.ReadAsStringAsync()), "FalhaLogin");
-                     
+                        );
+                var response = await Client.PostAsync("/login", campos);
+                if (response.IsSuccessStatusCode)
+                    MessagingCenter.Send(new Usuario(), "LoginSucesso");
+                else
+                    MessagingCenter.Send(new LoginException("Erro ao autenticar o usuário \n" + await response.Content.ReadAsStringAsync()), "FalhaLogin");
+            }
+            catch 
+            {
+                MessagingCenter.Send(
+                    new LoginException("Ocorreu um erro de comunicação com o servidor \n" + "Por favor, Verifique sua conexão e tente mais tarde!"),
+                    "FalhaLogin");
+            }
         }
     }
 }
